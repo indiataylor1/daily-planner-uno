b9dbc68e21dca7f974f7749ca988109f40e6f9a0
India Taylor <116799866+indiataylor1@users.noreply.github.com>
2022-12-08 22:21:23 +0000

    Updated JavaScript
    JavaScript updated event listener added, hour colors updated

--- script.js
+++ script.js
@@ -6,8 +6,44 @@ console.log(dayJsObject.format("D/M/YYYY H:mm"));
 
 document.getElementById("dateAndTime").innerHTML = dayJsObject;
 
-//event listener//
-document.getElementById("btn saveBtn col-2 col-md-1").addEventListener("click", save);
+//Event Listener save button
+$('.saveBtn').on('click', function() {
+  var textValue = $(this).siblings('.description').val();
+  var timeKey = $(this).parent().attr('id')
+
+  localStorage.setItem(timeKey, textValue);
+});
+
+// local storage
+$('#hour9 .description').val(localStorage.getItem('hour9'));
+$('#hour10 .description').val(localStorage.getItem('hour10'));
+$('#hour11 .description').val(localStorage.getItem('hour11'));
+$('#hour12 .description').val(localStorage.getItem('hour12'));
+$('#hour13 .description').val(localStorage.getItem('hour13'));
+$('#hour14 .description').val(localStorage.getItem('hour14'));
+$('#hour15 .description').val(localStorage.getItem('hour15'));
+$('#hour16 .description').val(localStorage.getItem('hour16'));
+$('#hour17 .description').val(localStorage.getItem('hour17'));
+
+//color change for past present future events
+function auditTask(){
+  var currentHour = today.hours();
+  $('.time-block').each(function () {
+    var timeId = parseInt($(this).attr('id').split("hour")[1]);
+  })
+}
+//this comment go here
+else {
+  $(this).removeClass('past');
+  $(this).removeClass('present');
+  $(this).removeClass('future');
+}
+//this comment go here 
+else {
+  $(this).removeClass('past');
+  $(this).removeClass('present');
+  $(this).removeClass('future');
+}
 
 
 
@@ -37,5 +73,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
 });
-
 
